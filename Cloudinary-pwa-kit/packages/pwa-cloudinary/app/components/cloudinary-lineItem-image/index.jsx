e8b44c6197fad81dc4d58904b6f9eea80ca04091
Asad Saleem <asaleem@rafter.one>
2024-03-06 19:07:11 +0500

    update logic

--- Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-lineItem-image/index.jsx
+++ Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-lineItem-image/index.jsx
@@ -6,27 +6,20 @@ import { AspectRatio, Box, Image } from '@chakra-ui/react'
  * The image gallery displays The Image Gallery Coming from Cloudinary in Product-Detail Page.
  */
 const CloudinaryLineItemImage = ({ cldProduct = {}, image = {} }) => {
-    const [url, setUrl] = useState(null)
-    if (cldProduct.miniCartImage && cldProduct.miniCartImage.url) { // Render CLD Image URL for Mini Cart
-        setUrl(cldProduct.miniCartImage.url)
-    } else if (cldProduct.url) { // Render CLD Image URL for (Cart Checkout Order Confirmation Order History and Order Detail)
-        setUrl(cldProduct.url)
-    }
-
     return (
         <>
-            {cldProduct && cldProduct.miniCartImage && url ? (
+            {cldProduct?.miniCartImage?.url ? (
                 <Box w="24" flex="none">
                     <AspectRatio ratio="1">
-                        <img src={url} alt={image && image.alt ? image.alt : null} />
+                        <img src={cldProduct?.miniCartImage?.url} alt={image && image.alt ? image.alt : null} />
                     </AspectRatio>
                 </Box>
             ) : (
                 <>
-                    {cldProduct.url && (
+                    {cldProduct?.url && (
                         <Image
                             alt={image.alt}
-                            src={`${url}`}
+                            src={`${cldProduct.url}`}
                             ignoreFallback={true}
                         />
                     )}
