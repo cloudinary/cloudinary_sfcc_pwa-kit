bbd4a083c92beab8754e7d96ecb6ea2e3bdb50ce
Asad Saleem <asaleem@rafter.one>
2024-07-09 19:08:10 +0500

    update the cloudinaryImageGallery

--- Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
+++ Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
@@ -5,16 +5,15 @@ import { cloudinary } from '../../../config/default'
 const RenderCloudinaryGalleryWidget = ({ cloudinaryImageGallery = {} }) => {
 
     useEffect(() => {
-        const cloudinaryObj = cloudinaryImageGallery
-        const gallery = document.querySelector('#cld-gallery');
-        while (gallery.firstChild) {
-            gallery.removeChild(gallery.firstChild)
-        }
-        if (cloudinaryObj && cloudinaryObj.galleryEnabled) {
-            let galleryOptions = cloudinaryObj.cloudinaryImage.galleryWidget.options
+        if (cloudinaryImageGallery && cloudinaryImageGallery.galleryEnabled) {
+            const galleryOptions = cloudinaryImageGallery.cloudinaryImage.galleryWidget.options
             galleryOptions.queryParam = cloudinary.CLD_PGW_TRACKING_PARAM
             if (typeof window !== 'undefined' && window.cloudinary && window.cloudinary.galleryWidget) {
-                const cldGallery = window.cloudinary.galleryWidget(galleryOptions)
+                if (window.cldGallery) {
+                    cldGallery.update(galleryOptions)
+                } else {
+                    window.cldGallery = window.cloudinary.galleryWidget(galleryOptions)
+                }
                 cldGallery.render()
             }
         }
