f3a43a9f22d087f45a2999cd743623c76fef43aa
Asad Saleem <asaleem@rafter.one>
2024-07-15 13:01:05 +0500

    added imageSrcset.js

---
+++ Cloudinary-pwa-kit/packages/pwa-cloudinary/app/utils/imageSrcset.js
@@ -0,0 +1,20 @@
+import { cloudinary } from '../../config/default'
+
+/**
+ * Upate the tracking params in srcsets
+ *
+ * @param {string} srcset - srcset.
+ * @returns {string} - The ImageGroup matching the search criteria
+ */
+export const updateTrackingParam = (srcset) => {
+    let splits = srcset.split(' ')
+    let newSrcset = splits.map((split) => {
+        if (split.lastIndexOf('?') > -1) {
+            return split.substring(0, split.lastIndexOf('?')) + cloudinary.CLD_TRACKING_PARAM
+        } else {
+            return split
+        }
+    })
+
+    return newSrcset.join(' ')
+}
