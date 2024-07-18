463cb36eaf98b48fb268003689654091aebf8138
Asad Saleem <asaleem@rafter.one>
2024-07-18 13:15:15 +0500

    update swatch rendering logic on PDP

--- Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
+++ Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
+import { cloudinary } from '../../../config/default'
 
 const RenderCloudinaryGalleryWidget = ({ cloudinaryImageGallery = {} }) => {
 
-    useEffect(() => {
-        const cloudinaryObj = cloudinaryImageGallery
-        if (cloudinaryObj && cloudinaryObj.galleryEnabled) {
-            const galleryOptions = cloudinaryObj.cloudinaryImage.galleryWidget.options
-            if (typeof window !== 'undefined' && window.cloudinary && window.cloudinary.galleryWidget) {
+    const renderGallery = async () => {
+        const gallery = document.querySelector('#cld-gallery')
+        const galleryOptions = cloudinaryImageGallery.cloudinaryImage.galleryWidget.options
+        galleryOptions.queryParam = cloudinary.CLD_PGW_TRACKING_PARAM
+        if (typeof window !== 'undefined' && window.cloudinary && window.cloudinary.galleryWidget) {
+            if (window.cldGallery) {
+                gallery.style.visibility = 'hidden'
+                await cldGallery.update(galleryOptions)
+                gallery.style.visibility = 'visible'
+            } else {
                 window.cldGallery = window.cloudinary.galleryWidget(galleryOptions)
-                cldGallery.update(galleryOptions)
                 cldGallery.render()
             }
+        }
+    }
+    
+    useEffect(() => {
+        if (cloudinaryImageGallery && cloudinaryImageGallery.galleryEnabled) {
+            renderGallery()
         }
     }, [cloudinaryImageGallery])
 
