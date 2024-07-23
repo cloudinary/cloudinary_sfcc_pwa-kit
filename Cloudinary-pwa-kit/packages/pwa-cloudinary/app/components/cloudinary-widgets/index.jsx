c3fb3ee6311a6299ec6558d2605bd30cb0f671d3
Asad Saleem <asaleem@rafter.one>
2024-07-23 13:31:37 +0500

    remove visibility attribute

--- Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
+++ Cloudinary-pwa-kit/packages/pwa-cloudinary/app/components/cloudinary-widgets/index.jsx
@@ -5,14 +5,11 @@ import { cloudinary } from '../../../config/default'
 const RenderCloudinaryGalleryWidget = ({ cloudinaryImageGallery = {} }) => {
 
     const renderGallery = async () => {
-        const gallery = document.querySelector('#cld-gallery')
         const galleryOptions = cloudinaryImageGallery.cloudinaryImage.galleryWidget.options
         galleryOptions.queryParam = cloudinary.CLD_PGW_TRACKING_PARAM
         if (typeof window !== 'undefined' && window.cloudinary && window.cloudinary.galleryWidget) {
             if (window.cldGallery) {
-                gallery.style.visibility = 'hidden'
                 await cldGallery.update(galleryOptions)
-                gallery.style.visibility = 'visible'
             } else {
                 window.cldGallery = window.cloudinary.galleryWidget(galleryOptions)
                 cldGallery.render()
